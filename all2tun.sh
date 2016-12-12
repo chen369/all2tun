780b3370d43f2661cf9dd4ccc1e09f0031dbf43e
Chen Chiu <chenchiu9@gmail.com>
2016-12-11 20:37:26 -0500

    Update all2tun.sh

--- all2tun.sh
+++ all2tun.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+
+
+#!/bin/bash
+socks_server=127.0.0.1:8080
+
+id="$RANDOM"
+tun="$(printf 'tun%04x' "$id")"
+ip tuntap add dev $tun mode tun
+ip link set $tun up
+ip addr add 169.254.1.1/30 dev $tun
+sysctl -w net.ipv4.conf.$tun.forwarding=1
+ip rule add fwmark $id lookup $id
+ip route add default via 169.254.1.2 table $id
+iptables -t mangle -I PREROUTING -i eth1 -p tcp -j MARK --set-mark $id
+iptables -t mangle -I PREROUTING -i eth2 -p tcp -j MARK --set-mark $id
+badvpn-tun2socks --tundev $tun --netif-ipaddr 169.254.1.2 --netif-netmask 255.255.255.252 --socks-server-addr $socks_server
+
+iptables -t mangle -D PREROUTING -i eth2 -p tcp -j MARK --set-mark $id
+iptables -t mangle -D PREROUTING -i eth1 -p tcp -j MARK --set-mark $id
+ip route del default via 169.254.1.2 table $id
+ip rule del from fwmark $id lookup $id
+ip tuntap del dev $tun mode tun
+
+
 ######################################################################################
 # version 0.05
 # 
